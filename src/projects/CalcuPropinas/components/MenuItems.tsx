e28929fa4ec067f2712f0238f58b30935c371436
Linnovel <linnovel_88@hotmail.com>
2024-08-24 22:02:20 -0400

    calcu-app

---
+++ src/projects/CalcuPropinas/components/MenuItems.tsx
@@ -0,0 +1,7 @@
+export const MenuItems = () => {
+  return (
+    <>
+      <div>MenuItems</div>
+    </>
+  );
+};
--- src/projects/CalcuPropinas/src/CalcuApp.tsx
+++ src/projects/CalcuPropinas/src/CalcuApp.tsx
@@ -1,4 +1,8 @@
+import { MenuItems } from "../components/MenuItems";
+import { menuItems } from "../datadb/data";
+
 const CalcuApp = () => {
+  console.log(menuItems);
   return (
     <>
       <header className="bg-teal-400 py-5">
@@ -6,6 +10,17 @@ const CalcuApp = () => {
           Calculara de propinas y consumo
         </h1>
       </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2 ">
+        <div>
+          <h2 className="font-medium">Menú</h2>
+          {menuItems.map((items) => (
+            <MenuItems key={items.id} />
+          ))}
+        </div>
+        <div>
+          <h2 className="font-medium ">Consumo</h2>
+        </div>
+      </main>
     </>
   );
 };
