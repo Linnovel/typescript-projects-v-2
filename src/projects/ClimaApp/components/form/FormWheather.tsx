ca0638442396fa3e9e43517fc915faf151619564
Linnovel <linnovel_88@hotmail.com>
2024-09-11 21:13:11 -0400

    weather-module

--- src/projects/ClimaApp/components/form/FormWheather.tsx
+++ src/projects/ClimaApp/components/form/FormWheather.tsx
@@ -1,17 +1,52 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { countries } from "../../data/indexClima";
 import styles from "./FormWheather.module.css";
+import type { SearchTypes } from "../../types";
 
 const FormWheather = () => {
+  const [search, setSearch] = useState<SearchTypes>({
+    city: "",
+    country: "",
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (Object.values(search).includes("")) {
+      console.log("hay campos vacios");
+    }
+  };
+
   return (
     <>
-      <form className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <div className={styles.field}>
           <label htmlFor="city">Ciudad</label>
-          <input id="city" type="text" name="city" placeholder="Ciudad" />
+          <input
+            id="city"
+            type="text"
+            name="city"
+            onChange={handleChange}
+            placeholder="Ciudad"
+            value={search.city}
+          />
         </div>
         <div className={styles.field}>
           <label htmlFor="pais">Pais:</label>
-          <select>
+          <select
+            name="country"
+            onChange={handleChange}
+            id="country"
+            value={search.country}
+          >
             <option value="">--Selecciona tu pais--</option>
             {countries.map((country) => (
               <option key={country.code} value={country.code}>
