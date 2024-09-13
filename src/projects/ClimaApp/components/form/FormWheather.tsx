d5f4e6cf5b3bd82300bcd4587d7bdb1eee46a48d
Linnovel <linnovel_88@hotmail.com>
2024-09-12 22:52:29 -0400

    zod validation

--- src/projects/ClimaApp/components/form/FormWheather.tsx
+++ src/projects/ClimaApp/components/form/FormWheather.tsx
@@ -1,68 +1,78 @@
-// import { ChangeEvent, FormEvent, useState } from "react";
-// import { countries } from "../../data/indexClima";
-// import styles from "./FormWheather.module.css";
-// // import type { SearchTypes } from "../../types";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { countries } from "../../data/indexClima";
+import styles from "./FormWheather.module.css";
+import type { SearchTypes } from "../../types";
+import Alert from "../Alert/Alert";
 
-// const FormWheather = () => {
-//   // const [search, setSearch] = useState<SearchTypes>({
-//   //   city: "",
-//   //   country: "",
-//   // });
+type FormProps = {
+  fetchWeather: (search: SearchTypes) => Promise<void>;
+};
 
-//   // const handleChange = (
-//   //   e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
-//   // ) => {
-//   //   setSearch({
-//   //     ...search,
-//   //     [e.target.name]: e.target.value,
-//   //   });
-//   // };
+const FormWheather = ({ fetchWeather }: FormProps) => {
+  const [search, setSearch] = useState<SearchTypes>({
+    city: "",
+    country: "",
+  });
 
-//   // const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-//   //   e.preventDefault();
-//   //   if (Object.values(search).includes("")) {
-//   //     console.log("hay campos vacios");
-//   //   }
-//   // };
+  const [alert, setAlert] = useState("");
 
-//   return (
-//     <>
-//       {/* <form onSubmit={handleSubmit} className={styles.form}> */}
-//         <div className={styles.field}>
-//           <label htmlFor="city">Ciudad</label>
-//           <input
-//             id="city"
-//             type="text"
-//             name="city"
-//             // onChange={handleChange}
-//             placeholder="Ciudad"
-//             // value={search.city}
-//           />
-//         </div>
-//         <div className={styles.field}>
-//           <label htmlFor="pais">Pais:</label>
-//           <select
-//             name="country"
-//             // onChange={handleChange}
-//             id="country"
-//             // value={search.country}
-//           >
-//             <option value="">--Selecciona tu pais--</option>
-//             {countries.map((country) => (
-//               <option key={country.code} value={country.code}>
-//                 {country.name}
-//               </option>
-//             ))}
-//           </select>
-//         </div>
-//         <input
-//           className={styles.submit}
-//           type="submit"
-//           value="Consultar Clima"
-//         />
-//       </form>
-//     </>
-//   );
-// };
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-// export default FormWheather;
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (Object.values(search).includes("")) {
+      setAlert("Todos los campos son obligatorios");
+      return;
+    }
+    fetchWeather(search);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.field}>
+          <label htmlFor="city">Ciudad</label>
+          <input
+            id="city"
+            type="text"
+            name="city"
+            onChange={handleChange}
+            placeholder="Ciudad"
+            value={search.city}
+          />
+        </div>
+        {alert && <Alert>{alert}</Alert>}
+        <div className={styles.field}>
+          <label htmlFor="pais">Pais:</label>
+          <select
+            name="country"
+            onChange={handleChange}
+            id="country"
+            value={search.country}
+          >
+            <option value="">--Selecciona tu pais--</option>
+            {countries.map((country) => (
+              <option key={country.code} value={country.code}>
+                {country.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <input
+          className={styles.submit}
+          type="submit"
+          value="Consultar Clima"
+        />
+      </form>
+    </>
+  );
+};
+
+export default FormWheather;
