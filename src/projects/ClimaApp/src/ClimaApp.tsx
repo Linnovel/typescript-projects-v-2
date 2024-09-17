9de3a80f0a928a6064f68cd5efad8d725472e12b
Linnovel <linnovel_88@hotmail.com>
2024-09-16 21:30:20 -0400

    clima-app done

--- src/projects/ClimaApp/src/ClimaApp.tsx
+++ src/projects/ClimaApp/src/ClimaApp.tsx
@@ -1,16 +1,19 @@
 import FormWheather from "../components/form/FormWheather";
+import Spinner from "../components/spinner/Spinner";
+import WeatherDetail from "../components/WeatherDetail/WeatherDetail";
 import useWeather from "../hooksAPiClima/useWeather";
 import styles from "./ClimaApp.module.css";
 
 const ClimaApp = () => {
-  const { fetchWeather } = useWeather();
+  const { weather, fetchWeather, hasWeatherData, loading } = useWeather();
 
   return (
     <>
       <h1 className={styles.title}>Buscando el clima</h1>
       <div className={styles.container}>
         <FormWheather fetchWeather={fetchWeather} />
-        <p>2</p>
+        {loading && <Spinner />}
+        {hasWeatherData && <WeatherDetail weather={weather} />}
       </div>
     </>
   );
