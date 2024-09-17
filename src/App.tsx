8d8a94b3c17372c7c7f46abf7d5e702d82920cdd
Linnovel <linnovel_88@hotmail.com>
2024-09-16 21:59:18 -0400

    cotizador-critpapp

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@
 // import AdminPacientes from "./projects/AdminPacientes/src/AdminPacientes";
 // import ControlGastosApp from "./projects/ControlGastos/src/ControlGastosApp";
 // import AppContador from "./projects/ContadorCalorias/src/AppContador";
+// import ClimaApp from "./projects/ClimaApp/src/ClimaApp";
 
-import ClimaApp from "./projects/ClimaApp/src/ClimaApp";
+import CriptoApp from "./projects/CotizadorCripto/src/CriptoApp";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <AppContador /> */}
       {/* <ControlGastosApp /> */}
       {/* <AdminPacientes /> */}
-      <ClimaApp />
+      {/* <ClimaApp /> */}
+      <CriptoApp />
     </>
   );
 }
---
+++ src/projects/CotizadorCripto/src/CriptoApp.tsx
@@ -0,0 +1,14 @@
+const CriptoApp = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="app-title">
+          Cotizador de <span>Criptomonedas</span>
+        </h1>
+        <div className="content"></div>
+      </div>
+    </>
+  );
+};
+
+export default CriptoApp;
