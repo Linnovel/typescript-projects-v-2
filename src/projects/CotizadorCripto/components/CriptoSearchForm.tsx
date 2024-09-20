fa6d542aa1e8e83062614e6bf6fa45bbe337493f
Linnovel <linnovel_88@hotmail.com>
2024-09-19 23:02:14 -0400

    zustand store next

--- src/projects/CotizadorCripto/components/CriptoSearchForm.tsx
+++ src/projects/CotizadorCripto/components/CriptoSearchForm.tsx
@@ -1,7 +1,36 @@
+import { currencies } from "../dataCripto";
+
 const CriptoSearchForm = () => {
   return (
     <>
-      <h1>cript search </h1>
+      <form className="form">
+        <div>
+          <div className="field">
+            <label className="curency" htmlFor="">
+              Moneda:{" "}
+            </label>
+            <select name="curency" id="curency">
+              <option value="">Seleccione--</option>
+              {currencies.map((currency) => (
+                <option key={currency.code} value={currency.code}>
+                  {currency.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div>
+          <div className="field">
+            <label className="criptcurrency" htmlFor="">
+              Criptomoneda:{" "}
+            </label>
+            <select name="criptcurrency" id="criptcurrency">
+              <option value="">Seleccione--</option>
+            </select>
+          </div>
+        </div>
+        <input type="submit" value="Cotizar" />
+      </form>
     </>
   );
 };
