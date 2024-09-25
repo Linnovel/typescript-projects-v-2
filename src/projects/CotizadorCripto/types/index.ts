a51f255a225539015755eed80b514df3720adf34
Linnovel <linnovel_88@hotmail.com>
2024-09-24 22:40:03 -0400

    crypto project done

--- src/projects/CotizadorCripto/types/index.ts
+++ src/projects/CotizadorCripto/types/index.ts
@@ -1,7 +1,7 @@
-import {CurrencySchema, CryptoCurrencyResponseSchema, PairSchemma} from '../schema/cripto-schema'
+import {CurrencySchema, CryptoCurrencyResponseSchema, PairSchemma, CryptoPriceSchema} from '../schema/cripto-schema'
 import {z} from 'zod'
 
 export type Currency = z.infer<typeof CurrencySchema>
 export type CryptoCurrency = z.infer<typeof CryptoCurrencyResponseSchema>
 export type Pair = z.infer<typeof PairSchemma>
-
+export type CryptoPrice = z.infer<typeof CryptoPriceSchema>
