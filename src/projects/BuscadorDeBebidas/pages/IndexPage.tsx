e1aece3c4a3ba582ce36a0b13b398f8013ae2b21
Linnovel <linnovel_88@hotmail.com>
2024-10-08 21:31:14 -0400

    favorite slice

--- src/projects/BuscadorDeBebidas/pages/IndexPage.tsx
+++ src/projects/BuscadorDeBebidas/pages/IndexPage.tsx
@@ -2,30 +2,24 @@ import { useMemo } from "react";
 import { useAppStore } from "../store/useAppStore";
 import DrinkCard from "../components/DrinkCard";
 
-
-
 const IndexPage = () => {
+  const drinks = useAppStore((state) => state.drinks);
 
-const drinks =  useAppStore((state) =>  state.drinks)
-console.log(drinks)
-
-  const hasDrinks = useMemo(() => drinks.drinks.length , [drinks]) 
+  const hasDrinks = useMemo(() => drinks.drinks.length, [drinks]);
 
   return (
     <>
       <h1 className="text-6xl font-extrabold">Recetas</h1>
       {hasDrinks ? (
         <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
-        {drinks.drinks.map((drink) => (
-          <DrinkCard
-          key={drink.idDrink}
-          drink={drink}
-          />
-        ))}
+          {drinks.drinks.map((drink) => (
+            <DrinkCard key={drink.idDrink} drink={drink} />
+          ))}
         </div>
-
-      ): (
-        <p className="my-10 text-center text-2xl">No hay resutados aun, utiliza el formulario para buscar recetas</p>
+      ) : (
+        <p className="my-10 text-center text-2xl">
+          No hay resutados aun, utiliza el formulario para buscar recetas
+        </p>
       )}
     </>
   );
--- src/projects/BuscadorDeBebidas/typesDrink/index.ts
+++ src/projects/BuscadorDeBebidas/typesDrink/index.ts
@@ -1,9 +1,12 @@
 import {z } from 'zod'
 
-import { CategoriesAPIResponseSchema, SearchFilterSchema, DrinksApiResponse, DrinkApiResponse } from '../UtilsDrinks/recipes-shemma';
+//Aca se esta validando las schema y respuestas
+
+import { CategoriesAPIResponseSchema, SearchFilterSchema, DrinksApiResponse, DrinkApiResponse, RecipeAPIResponseSchema } from '../UtilsDrinks/recipes-shemma';
 
 
 export type CategoryDrink = z.infer<typeof CategoriesAPIResponseSchema>
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drinks = z.infer<typeof DrinksApiResponse>
 export type Drink = z.infer<typeof DrinkApiResponse>
+export type Recipe = z.infer<typeof RecipeAPIResponseSchema>
